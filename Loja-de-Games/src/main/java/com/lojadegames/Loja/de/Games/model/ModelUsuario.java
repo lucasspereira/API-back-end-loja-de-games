f9e9a82f86c41dafabb529ea9a4a7a8c7f0b5f2a
Lucas Pereira <pereira.lucas23@gmail.com>
2021-02-26 15:38:04 -0300

    Model Usuário

---
+++ Loja-de-Games/src/main/java/com/lojadegames/Loja/de/Games/model/ModelUsuario.java
@@ -0,0 +1,31 @@
+package com.lojadegames.Loja.de.Games.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name = "usuario")
+public class ModelUsuario {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+	@NotNull @Size (min = 3, max = 100) private String nome;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
